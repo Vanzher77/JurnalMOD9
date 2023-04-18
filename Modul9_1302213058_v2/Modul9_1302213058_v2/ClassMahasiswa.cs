872ce371549cc558be1ff84ae3fe6047a4433f7c
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-04-18 16:56:42 +0700

    Menambahkan API Mahasiswa

---
+++ Modul9_1302213058_v2/Modul9_1302213058_v2/ClassMahasiswa.cs
@@ -0,0 +1,20 @@
+﻿namespace Modul9_1302213058_v2
+{
+    public class Mahasiswa
+    {
+        public string Name { get; set; }
+        public string Nim { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+            this.Name = name;
+            this.Nim = nim;
+            this.Course = course;
+            this.Year = year;
+
+        }
+
+        public Mahasiswa () { }
+    }
+}
---
+++ Modul9_1302213058_v2/Modul9_1302213058_v2/Controllers/MahasiswaController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modul9_1302213058_v2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        public static List<Mahasiswa> mahasiswa = new List<Mahasiswa>
+        {
+            //pada parameter course terdapat array list, saya menggunakan course = new List<string> untuk menginisialisasi sebuah objek List sebelum digunakan
+            new Mahasiswa{Name = "Mochamad Rizky Evan Ramadhan", Nim = "1302213058", Course = new List<string> { "Konstruksi Perangkat Lunak","UX" }, Year = 2021 },
+            new Mahasiswa{Name = "Kamal Maula Azka Shidqi", Nim = "1302210032", Course = new List<string>{ "Basis Data","UI"}, Year = 2022 },
+            new Mahasiswa{Name = "Ignaj nadhif", Nim = "1302213058", Course = new List<string>{ "Pemrograman Berorientasi Objek","Proting"}, Year = 2023 }
+        };
+
+        [HttpGet]
+        //membuat output list array dari objek mahasiswa 
+        public IEnumerable<Mahasiswa> Get() 
+        {
+            return mahasiswa;
+        }
+
+        [HttpPost]
+        //membuat objek mahasiswa baru dan menambahkan objek menyertai nama, nim, matakuliah dan tahun
+        public void post([FromBody] Mahasiswa newMahasiswa)
+        {
+            mahasiswa.Add(newMahasiswa);
+        }
+
+        [HttpGet("index")]
+        //memanggil 1 objek mahasiswa menggunakan index array
+        public Mahasiswa Get(int  index) 
+        {
+            return mahasiswa[index];
+        }
+
+        [HttpDelete("index")]
+        //menghapus objek mahasiswa sesuai dengan array listnya
+        public void Delete(int index)
+        {
+            mahasiswa.RemoveAt(index); 
+        }   
+
+    }
+}
